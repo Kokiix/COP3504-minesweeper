d262b39389d65c94aeaf3dedfd68db6df354c260
Kenneth <kokiidoki@gmail.com>
2025-11-13 16:54:44 -0500

    draw tile sprite to screen

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -2,6 +2,10 @@
 
 #include "SFML/Window/VideoMode.hpp"
 #include "SFML/Window/Window.hpp"
+#include <SFML/Graphics/Texture.hpp>
+
+#include "SFML/Graphics/RenderWindow.hpp"
+#include "SFML/Graphics/Sprite.hpp"
 
 void GameInstance::read_config_file() {
     // TODO
@@ -10,13 +14,22 @@ void GameInstance::read_config_file() {
     n_cols = 10;
 }
 
+void GameInstance::load_image_assets() {
+    tile_texture = sf::Texture("../assets/images/tile_hidden.png", false, sf::IntRect({10, 10}, {50, 50}));
+}
+
 void GameInstance::welcome_loop() {
     // TODO
 }
 
 void GameInstance::game_loop() {
-    sf::Window window(sf::VideoMode({static_cast<unsigned>(32 * n_rows + 100), static_cast<unsigned>(n_cols * 32)}), "Minesweeper");
+    sf::RenderWindow window(sf::VideoMode({static_cast<unsigned>(32 * n_rows + 100), static_cast<unsigned>(n_cols * 32)}), "Minesweeper", sf::Style::Close);
+    sf::Sprite test(tile_texture);
+    test.setPosition(sf::Vector2f(50, 50));
     while (window.isOpen()) {
+        window.clear();
+        window.draw(test);
+        window.display();
         while (const std::optional event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) window.close();
         }
--- minesweeper.h
+++ minesweeper.h
@@ -1,16 +1,22 @@
 #pragma once
 
 #include <cstdio>
+#include <SFML/Graphics/Texture.hpp>
 
 class GameInstance {
     size_t n_rows;
     size_t n_cols;
+    sf::Texture tile_texture;
+
     void read_config_file();
+    void load_image_assets();
     void welcome_loop();
     void game_loop();
 public:
     GameInstance() {
         read_config_file();
+        load_image_assets();
+
         welcome_loop();
         game_loop();
     }
